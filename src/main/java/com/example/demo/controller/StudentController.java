c3384c198afd8d197562af8108b9322bb02d81c6
bala-amypo <bala@amypo.in>
2025-12-17 05:06:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,6 +1,6 @@
-package com.example.demo.controller
+package com.example.demo.
 
-import java.util.List
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -10,5 +10,16 @@ public class StudentService{
         details.put(student.getId(),student);
         return student;
     }
-    public java.util.List<St>
+    public java.util.List<StudentEntity>getAllStudentEntity(){
+        return new java.util.ArrayList<>(details.values();)
+    }
+    public void deleteStudentById(int id){
+        details.remove(id);
+    }
+    public java.util.Optional<StudentEntity> getStudentById(int id){
+        return java.util.Optional.ofNullable(details.get(id));
+    }
+    public void insertStudent(StudentEntity student){
+        details.put(student.getId(),student);
+    }
 }
